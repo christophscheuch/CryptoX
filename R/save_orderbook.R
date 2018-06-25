74efdc9af0fe6db80c9b86759da960ba797a6cfc
ckscheuch <christoph.scheuch@gmail.com>
2018-06-25 15:25:55 +0200

    Added "path" as new option

--- R/save_orderbook.R
+++ R/save_orderbook.R
@@ -9,24 +9,34 @@
 
 save_orderbook <- function(exchange = as.character(NA),
                            asset_pair = as.character(NA),
-                           level = 10, ob = NA,
+                           level = 10,
+                           path = NA,
+                           ob = NA,
                            filetype = "rds",
-                           print.output = FALSE){
+                           print.output = FALSE) {
   if(is.na(ob)) {
     ob <- get_orderbook(exchange = exchange,
                         asset_pair = asset_pair,
                         level = level)
   }
-  if(print.output) print(ob)
-  dir.create(file.path(exchange), showWarnings = FALSE)
+
+  if (print.output) print(ob)
+
+  if (!is.na(path)) {
+    dir.create(file.path(path, exchange), showWarnings = FALSE, recursive = TRUE)
+    path <- file.path(path, exchange)
+  } else {
+    dir.create(file.path(exchange), showWarnings = FALSE)
+    path <- exchange
+  }
 
   if (filetype == "rds") {
-    saveRDS(ob, file = paste0(exchange, "/",
+    saveRDS(ob, file = paste0(path, "/",
                               asset_pair, "_orderbook_", ob$timestamp, ".rds"))
   }
 
   if (filetype == "RData") {
-    save(ob, file = paste0(exchange, "/",
+    save(ob, file = paste0(path, "/",
                               asset_pair, "_orderbook_", ob$timestamp, ".RData"))
   }
 }
